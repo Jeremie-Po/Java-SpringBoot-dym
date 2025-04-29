7040b1287f5f6cc4e0b3d76a3c09f4cc14cf782f
Jérémie P <jpourageaux@hotmail.fr>
2025-04-29 16:25:28 +0200

    add repository to get the data - it is a statut on this example

--- src/main/java/com/LearningSpring/tennis/service/HealthCheckService.java
+++ src/main/java/com/LearningSpring/tennis/service/HealthCheckService.java
@@ -1,14 +1,24 @@
 package com.LearningSpring.tennis.service;
 
+import com.LearningSpring.tennis.repository.HealthCheckRepository;
 import com.LearningSpring.tennis.rest.ApplicationStatus;
 import com.LearningSpring.tennis.HealthCheck;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
 
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+
     public HealthCheck healthCheck() {
-        return new HealthCheck(ApplicationStatus.OK, "welcome to tennis application");
+        Long activeSessions = healthCheckRepository.countApplicationConnections();
+
+        if (activeSessions > 0) {
+            return new HealthCheck(ApplicationStatus.OK, "welcome to tennis application");
+        }
+        return new HealthCheck(ApplicationStatus.KO, "no active connections");
     }
 
 }
