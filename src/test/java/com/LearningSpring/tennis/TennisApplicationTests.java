85e301353e32e5b840092151afc20996293a0158
Jérémie P <jpourageaux@hotmail.fr>
2025-04-29 18:03:03 +0200

    add basic test

--- src/test/java/com/LearningSpring/tennis/TennisApplicationTests.java
+++ src/test/java/com/LearningSpring/tennis/TennisApplicationTests.java
@@ -1,13 +1,20 @@
 package com.LearningSpring.tennis;
 
+import com.LearningSpring.tennis.service.HealthCheckService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TennisApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private HealthCheckService healthCheckService;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertNotNull(healthCheckService);
+    }
 
 }
