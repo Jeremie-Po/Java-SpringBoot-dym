23657904d6adf8c831d5dda10bc6cd43e159b5ba
Jérémie P <jpourageaux@hotmail.fr>
2025-04-30 18:24:28 +0200

    create a players api

--- src/main/java/com/LearningSpring/tennis/web/PlayerController.java
+++ src/main/java/com/LearningSpring/tennis/web/PlayerController.java
@@ -8,15 +8,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 
 @Tag(name = "Tennis Players API")
 @RestController
+@RequestMapping("/players")
 public class PlayerController {
 
     @Operation(summary = "finds players", description = "find players")
@@ -28,21 +27,34 @@ public class PlayerController {
                     })
     })
 
-    @GetMapping("/players")
+    @GetMapping
     public List<Player> list() {
         return Collections.emptyList();
     }
 
     @Operation(summary = "find a player", description = "find a player")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "players list",
+            @ApiResponse(responseCode = "200", description = "player",
                     content = {
                             @Content(mediaType = "application/json",
                                     schema = @Schema(implementation = Player.class))
                     })
     })
-    @GetMapping("/players/{lastName}")
+    @GetMapping("{lastName}")
     public Player getByLastName(@PathVariable("lastName") String lastName) {
         return null;
+    }
+
+    @Operation(summary = "Add a player", description = "add a player")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "created player",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Player.class))
+                    })
+    })
+    @PostMapping
+    public Player createPlayer(@RequestBody Player player) {
+        return player;
     }
 }
