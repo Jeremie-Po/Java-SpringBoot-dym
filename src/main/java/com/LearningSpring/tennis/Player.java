e59b4859b9fe6b9ffa831cf42ce1b798bca53f27
Jérémie P <jpourageaux@hotmail.fr>
2025-05-01 14:07:42 +0200

    error handle

--- src/main/java/com/LearningSpring/tennis/Player.java
+++ src/main/java/com/LearningSpring/tennis/Player.java
@@ -2,14 +2,16 @@ package com.LearningSpring.tennis;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
 public record Player(
+//        @NotBlank(message = "First name is mandator") String firstName, // to customize message
         @NotBlank String firstName,
         @NotBlank String lastName,
-        @PastOrPresent LocalDate birthDate,
+        @NotNull @PastOrPresent LocalDate birthDate,
         @Valid Rank rank
 ) {
 }
--- src/main/java/com/LearningSpring/tennis/web/PlayerControllerErrorHandler.java
+++ src/main/java/com/LearningSpring/tennis/web/PlayerControllerErrorHandler.java
@@ -1,10 +1,14 @@
 package com.LearningSpring.tennis.web;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
@@ -13,6 +17,17 @@ public class PlayerControllerErrorHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleNoElementException() {
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> HandleValidationException(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
